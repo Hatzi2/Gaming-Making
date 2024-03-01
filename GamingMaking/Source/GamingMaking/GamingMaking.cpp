e9228ebe95fdddd113a7ad298d4cc80b488b0037
Hatzi2 <jonas.lind10@gmail.com>
2024-03-01 09:19:41 +0100

    switching to 5.3 again

--- GamingMaking/Source/GamingMaking/GamingMaking.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GamingMaking.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GamingMaking, "GamingMaking" );
- 
